86502a5e4898a80ea6bd7d31b3be2f63df011b7f
CJ Hwang <xanthoc68@gmail.com>
2019-08-10 06:34:51 +0900

    debugger on

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
 from flask import Flask
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
 @app.route("/")
 @app.route("/hello")
 def hello_world():
-	return "Hello, world!"
+	return "Hello, world! I'm I."
 
-@app.route("/users")
-def users():
-	return "CJ"
+@app.route("/test/<search_query>")
+def search(search_query):
+	return search_query
+
+@app.route("/name/<name>")
+def index(name):
+	if name=="michael":
+		return "Hello, {}".format(name), 200
+	else:
+		return "Not Found", 404
 
 if __name__ == "__main__":
 	app.run()
